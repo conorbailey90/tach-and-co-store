abf0e903cfe2266b8f153915db43f84e452de6d9
conorbailey90 <conbailey90@gmail.com>
2022-03-02 16:24:25 +0000

    Revalidate added to hompage for automatic refresh on content update

--- pages/index.js
+++ pages/index.js
@@ -73,5 +73,6 @@ export async function getStaticProps(context) {
 
   return {
     props: { homePageTiles }, // will be passed to the page component as props
+    revalidate: 10, // In seconds
   }
 }
